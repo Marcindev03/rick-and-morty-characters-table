41a5b9049e2b91d63d5566d36ec2854402020f5e
Marcin Sacha <marcindev03@gmail.com>
2022-12-04 15:33:23 +0100

    Use custom font

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import { ChakraProvider } from "@chakra-ui/react";
 
 import App from "./App";
 
+import "@fontsource/oswald/400.css";
+import "@fontsource/oswald/500.css";
+import "@fontsource/oswald/700.css";
 import { theme } from "./styles";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,10 @@
 import { extendTheme } from "@chakra-ui/react";
 
-const config = {};
+const config = {
+  fonts: {
+    heading: `'Oswald', sans-serif`,
+    body: `'Oswald', sans-serif`,
+  },
+};
 
 export const theme = extendTheme(config);
