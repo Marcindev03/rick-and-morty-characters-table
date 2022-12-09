09e8af804ec05e53842653ad7dc9b47fd38fcfa0
Marcin Sacha <marcindev03@gmail.com>
2022-12-09 10:23:53 +0100

    Include all species in filters

--- src/mocks/data.ts
+++ src/mocks/data.ts
@@ -14,6 +14,34 @@ export const SPECIES: Option[] = [
     label: "Humanoid",
     value: "humanoid",
   },
+  {
+    label: "unknown",
+    value: "unknown",
+  },
+  {
+    label: "Poopybutthole",
+    value: "poopybutthole",
+  },
+  {
+    label: "Mythological Creature",
+    value: "mythological creature",
+  },
+  {
+    label: "Animal",
+    value: "animal",
+  },
+  {
+    label: "Robot",
+    value: "robot",
+  },
+  {
+    label: "Cronenberg",
+    value: "cronenberg",
+  },
+  {
+    label: "Disease",
+    value: "disease",
+  },
 ];
 
 export const DEFAULT_FILTERS: CharacterFilter = {
